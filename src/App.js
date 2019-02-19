c42f48aa9dc39515901dd0361af487f354e0fb14
ryanboris <ryan.boris@gmail.com>
2019-02-18 20:22:16 -0500

    initial commit on new branch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
-import React, { useContext, useReducer } from 'react'
-import Table from './Table'
-import Form from './Form'
-import CharactersContext from './context'
-import reducer from './reducer'
+import React from 'react'
 
 export default function App() {
-  const initialState = useContext(CharactersContext)
-  const [state, dispatch] = useReducer(reducer, initialState)
-  return (
-    <CharactersContext.Provider value={{ state, dispatch }}>
-      <Table />
-      <Form />
-    </CharactersContext.Provider>
-  )
+  return <div />
 }
